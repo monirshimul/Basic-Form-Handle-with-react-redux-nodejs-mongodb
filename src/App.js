4287d7d67aef4c26fbf1279c531bc0e4b3dabc93
monirshimul <monirshimul@gmail.com>
2020-02-26 16:49:23 +0600

    backend added in this project===simple form handle with react-redux

--- src/App.js
+++ src/App.js
@@ -1,41 +1,33 @@
 import React from 'react';
-import { useSelector, useDispatch, shallowEqual } from "react-redux";
-import { increment, decrement } from './actions/counterAction';
+import { useSelector, shallowEqual } from "react-redux";
 import OtherPage from "./OtherPage";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Interaction from './Interaction';
+
 
 import Form from './components/Form';
 
 
 function App(props) {
-  const counter = useSelector(state => state.counter);
-  const auth = useSelector(state => state.auth);
+
 
   const { name, org, des } = useSelector(state => ({
-    name: state.FormReducer.name,
-    org: state.FormReducer.organization,
-    des: state.FormReducer.designation
+    name: state.Form.name,
+    org: state.Form.organization,
+    des: state.Form.designation
   }), shallowEqual);
 
-  const dispatch = useDispatch();
-  console.log(props);
   return (
     <Router>
       <div className="App">
         <Switch>
           <Route path="/other" exact component={OtherPage} />
-          <Route path="/login" exact component={Form} />
+          <Route path="/" exact component={Form} />
         </Switch>
-        <Interaction />
-        <h1>Counter {counter}</h1>
-        {console.log("This is App Render: ")}
+
         <h1>Name : {name}</h1>
         <h1>Organization : {org}</h1>
         <h1>Designation : {des}</h1>
-        <button onClick={() => dispatch(increment(5))}>+</button>
-        <button onClick={() => dispatch(decrement(5))}>-</button>
-        {auth ? <h3>You are logged in</h3> : <h3>your are not logged in</h3>}
+
 
 
       </div>
--- src/reducer/allReducer.js
+++ src/reducer/allReducer.js
@@ -10,7 +10,7 @@ const allReducer = combineReducers({
     counter: counterReducer,
     auth: authReducer,
     another: AnotherReducer,
-    FormReducer
+    Form: FormReducer
 })
 
 
